36210c97ac334a080ba86a71a4833afa208b9d36
Ricka Ortiz <rickairis.ortiz@gmail.com>
2020-11-29 15:54:42 +0000

    Tinkering file

---
+++ tinkering.js
@@ -0,0 +1,53 @@
+//BANK FLOW ACTIVITIES
+// var balance = 325.00;
+// var checkBalance = true;
+// var isActive = false;
+
+// if (isActive === true && checkBalance === true) {
+//   if (balance > 0) {
+//     console.log(`Your balance is $${balance}.`)
+//   } else if (balance ==== 0) {
+//     console.log("Your account is empty.");
+//   } else {
+//     console.log("Your balance is negative, please contact bank.");
+//   }
+// } elseif (isActive === false && checkBalance === true){
+//  console.log("Your account is no longer active.")
+// } else {console.log ("Thank you, have a nice day!");
+// }
+
+
+//FLAVORS EXERCISE
+// var flavor = "strawberry";
+// var vessel = "cone";
+// var toppings = "cookies";
+
+
+// if (flavor === "vanilla" || flavor === "chocolate")
+// { if ((vessel === "cone" || vessel ==="bowl") && (toppings === "peanuts" || toppings ==="sprinkles")) {
+//   console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}`)
+// }
+
+// }
+
+//SHIRT SIZE ACTIVITY
+// var shirtWidth = 22;
+// var shirtLength = 30;
+// var shirtSleeve = 8.63;
+
+// if ((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)) {
+//   console.log("S");
+// } else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)){
+//   console.log("M"); 
+// } else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)) {
+//   console.log("L");
+// } else if ((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)) {
+//   console.log("XL");
+// } else if ((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)) {
+//   console.log("2XL");
+// } else if ((shirtWidth >= 28) && (shirtLength >= 34) && (shirtSleeve >= 10.13)) {
+//   console.log("3XL")
+// } else {
+//   console.log("N/A")
+// }
+  
