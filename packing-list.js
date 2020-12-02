645d330f71723a6666877428da9e791a460bd4e3
Ricka Ortiz <rickairis.ortiz@gmail.com>
2020-12-02 23:26:39 +0000

    using loops to iterate over packing list

--- packing-list.js
+++ packing-list.js
@@ -2,6 +2,15 @@ const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glas
 
 console.log("Kitchen stuff to pack:");
 
-for(let i = 0; i < packingList.length; i++){
+//using while..loop
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
+
+//using for.. loop
+// for(let i = 0; i < packingList.length; i++){
+//   console.log(packingList[i]);
+// }
+
